7d299d262800dd1b5d2d514bd92ed2ba31e68616
Vaibhav Agrawal <90774825+vaibhav514@users.noreply.github.com>
2023-05-22 00:33:53 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,49 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+// Input Format
+
+// First Line array size
+// Second Line elements of array
+// Constraints
+
+// 1 <= nums.length <= 3 * 104
+// -3 * 104 <= nums[i] <= 3 * 104
+// Each element in the array appears twice except for one element which appears only once.
+// Output Format
+
+// Unique Number present in the array
+// Sample Input 0
+
+// 3
+// 2 2 1
+// Sample Output 0
+
+// 1
+// Sample Input 1
+
+// 5
+// 2 2 3 3 4
+// Sample Output 1
+
+// 4
+// **********************************************************************************************************************************************************
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int{} g = new int[i];
+        for(int f = 0; f<i; f++) {
+            g[f] = sc.nextInt();
+        }
+        for(int f = 0; f<i; f++) {
+            for(int e = 0; e<i; e++) {
+                if(g[f]==g[e]){
+                    
+                }
+        }
+        }
+}
